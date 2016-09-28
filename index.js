db7299cc6d7fa2cc8f62d914312f4295a11afc71
Antti <anttis.pitkanen@gmail.com>
2016-09-28 23:37:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Document.getElement
+
+document.getElementById('contact').onclick=() => {
+    document.getElementById('contact').classList.toggle('expanded');
+    document.getElementById('contact-info').classList.toggle('shown');
+    document.getElementById('contact-close').classList.toggle('close-shown');
+    document.getElementById('contact-icon').classList.toggle('icon-hidden');
+};
+
+document.getElementById('music').onclick=() => {
+    document.getElementById('music').classList.toggle('expanded');
+    document.getElementById('music-info').classList.toggle('shown');
+    document.getElementById('music-close').classList.toggle('close-shown');
+    document.getElementById('music-icon').classList.toggle('icon-hidden');
+};
