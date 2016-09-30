7e116f863ca6804205e33b2ac660dfcef6a3595c
Antti <anttis.pitkanen@gmail.com>
2016-10-01 00:33:01 +0300

    let's call it a night

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
-//document.getElementById('heading').onclick=() => {
-//    console.log("jou");
-//};
-
-let isExpanded = false;
-console.log("funktioiden ulkopuolella ollaan että " + isExpanded);
-
+/*
 let addClassIfNotPresent = function(id, addedClass) {
     if(!document.getElementById(id).classList.contains(addedClass)) {
         document.getElementById(id).classList.add(addedClass);
     }
 }
+*/
 
+//receives array of arrays with [id, class]
+function toggleClass(arrayOfArrays) {
+    for(let i=0; i<arrayOfArrays.length; i++) {
+        let id = arrayOfArrays[i][0];
+        let addedClass = arrayOfArrays[i][1];
 
-document.getElementById('contact').onclick=() => {
-    //if(!isExpanded) {
-        addClassIfNotPresent('contact', 'expanded');
-        addClassIfNotPresent('contact-info', 'shown');
-        addClassIfNotPresent('contact-close', 'close-shown');
-        addClassIfNotPresent('contact-icon', 'icon-hidden');
-        //isExpanded = true;
-        console.log("avataan " + isExpanded);
+        if(!document.getElementById(id).classList.contains(addedClass)) {
+            document.getElementById(id).classList.add(addedClass);
+        } else {
+            document.getElementById(id).classList.remove(addedClass);
+        }
+    }
+}
+/*
+function addClassIfNotPresent(arrayOfArrays) {
+    for(let i=0; i<arrayOfArrays.length; i++) {
+        let id = arrayOfArrays[i][0];
+        let addedClass = arrayOfArrays[i][1];
 
-        document.getElementById('contact-close').onclick=() => {
-            document.getElementById('contact').classList.toggle('expanded');
-            document.getElementById('contact-info').classList.toggle('shown');
-            document.getElementById('contact-close').classList.toggle('close-shown');
-            document.getElementById('contact-icon').classList.toggle('icon-hidden');
-            console.log("ruksiin osui :D");
-            //isExpanded = false;
-        };
-    //};
+        if(!document.getElementById(id).classList.contains(addedClass)) {
+            document.getElementById(id).classList.add(addedClass);
+        }
+    }
+}
+*/
+document.getElementById('contact-icon').onclick=() => {
+    toggleClass([
+        ["contact", "expanded"],
+        ["contact-info", "shown"],
+        ["contact-close", "close-shown"],
+        ["contact-icon", "icon-hidden"]
+    ]);
 };
+
 
 /*
-if (!isExpanded) {
-    document.getElementById('contact').onclick=() => {
-        document.getElementById('contact').classList.toggle('expanded');
-        document.getElementById('contact-info').classList.toggle('shown');
-        document.getElementById('contact-close').classList.toggle('close-shown');
-        document.getElementById('contact-icon').classList.toggle('icon-hidden');
-        isExpanded = true;
-        console.log(isExpanded);
-    }
+document.getElementById('contact-icon').onclick=() => {
+    addClassIfNotPresent('contact', 'expanded');
+    addClassIfNotPresent('contact-info', 'shown');
+    addClassIfNotPresent('contact-close', 'close-shown');
+    addClassIfNotPresent('contact-icon', 'icon-hidden');
+    console.log("avataan " + isExpanded);
+
 };
 */
+
+document.getElementById('contact-close').onclick=() => {
+    toggleClass([
+        ["contact", "expanded"],
+        ["contact-info", "shown"],
+        ["contact-close", "close-shown"],
+        ["contact-icon", "icon-hidden"]
+    ]);
+    /*
+    document.getElementById('contact').classList.toggle('expanded');
+    document.getElementById('contact-info').classList.toggle('shown');
+    document.getElementById('contact-close').classList.toggle('close-shown');
+    document.getElementById('contact-icon').classList.toggle('icon-hidden');
+    */
+};
 
 
 
