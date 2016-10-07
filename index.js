f1eee54a075a066e7085ed35078b77d52fcf8db3
Antti <anttis.pitkanen@gmail.com>
2016-10-07 14:49:05 +0300

    downgrading from es6 for better compatibility, silly me

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 //receives array of arrays with [id, class]
 function toggleClass(arrayOfArrays) {
-    for(let i=0; i<arrayOfArrays.length; i++) {
-        let id = arrayOfArrays[i][0];
-        let addedClass = arrayOfArrays[i][1];
+    for(var i=0; i<arrayOfArrays.length; i++) {
+        var id = arrayOfArrays[i][0];
+        var addedClass = arrayOfArrays[i][1];
 
         if(!document.getElementById(id).classList.contains(addedClass)) {
             document.getElementById(id).classList.add(addedClass);
@@ -24,20 +24,20 @@ function toggleShown(topic) {
     ])
 }
 
-document.getElementById('contact-icon').onclick=() => { toggleShown("contact"); };
-document.getElementById('contact-close').onclick=() => { toggleShown("contact"); };
+document.getElementById('contact-icon').onclick=function() { toggleShown("contact"); };
+document.getElementById('contact-close').onclick=function() { toggleShown("contact"); };
 
-document.getElementById('cooperation-icon').onclick=() => { toggleShown("cooperation"); };
-document.getElementById('cooperation-close').onclick=() => { toggleShown("cooperation"); };
+document.getElementById('cooperation-icon').onclick=function() { toggleShown("cooperation"); };
+document.getElementById('cooperation-close').onclick=function() { toggleShown("cooperation"); };
 
-document.getElementById('music-icon').onclick=() => { toggleShown("music"); };
-document.getElementById('music-close').onclick=() => { toggleShown("music"); };
+document.getElementById('music-icon').onclick=function() { toggleShown("music"); };
+document.getElementById('music-close').onclick=function() { toggleShown("music"); };
 
-document.getElementById('writing-icon').onclick=() => { toggleShown("writing"); };
-document.getElementById('writing-close').onclick=() => { toggleShown("writing"); };
+document.getElementById('writing-icon').onclick=function() { toggleShown("writing"); };
+document.getElementById('writing-close').onclick=function() { toggleShown("writing"); };
 
-document.getElementById('community-icon').onclick=() => { toggleShown("community"); };
-document.getElementById('community-close').onclick=() => { toggleShown("community"); };
+document.getElementById('community-icon').onclick=function() { toggleShown("community"); };
+document.getElementById('community-close').onclick=function() { toggleShown("community"); };
 
-document.getElementById('coding-icon').onclick=() => { toggleShown("coding"); };
-document.getElementById('coding-close').onclick=() => { toggleShown("coding"); };
+document.getElementById('coding-icon').onclick=function() { toggleShown("coding"); };
+document.getElementById('coding-close').onclick=function() { toggleShown("coding"); };
