4c59884b6b0d65b27b252fb8443ff4d301390263
Antti <anttis.pitkanen@gmail.com>
2016-10-07 23:35:17 +0300

    added the ability to close a div with esc

--- index.js
+++ index.js
@@ -24,6 +24,35 @@ function toggleShown(topic) {
     ])
 }
 
+//for the esc key event
+function removeAClass(arrayOfArrays) {
+    for(var i=0; i<arrayOfArrays.length; i++) {
+        var id = arrayOfArrays[i][0];
+        var classToBeRemoved = arrayOfArrays[i][1];
+
+        if(document.getElementById(id).classList.contains(classToBeRemoved)) {
+            document.getElementById(id).classList.remove(classToBeRemoved);
+        }
+    }
+}
+
+//for removing all the shown classes on esc key event
+function removeShown() {
+    var classes = ["contact", "cooperation", "music", "wellbeing", "coding", "community"];
+    for(var i=0; i<classes.length; i++) {
+        var targetClass = classes[i];
+        removeAClass([
+            [targetClass, "expanded"],
+            [targetClass+"-info", "shown"],
+            [targetClass+"-close", "close-shown"],
+            [targetClass+"-icon", "icon-hidden"],
+            ["body", "body-open"]
+        ])
+    }
+
+}
+
+
 document.getElementById('contact-icon').onclick=function() { toggleShown("contact"); };
 document.getElementById('contact-close').onclick=function() { toggleShown("contact"); };
 
@@ -33,11 +62,6 @@ document.getElementById('cooperation-close').onclick=function() { toggleShown("c
 document.getElementById('music-icon').onclick=function() { toggleShown("music"); };
 document.getElementById('music-close').onclick=function() { toggleShown("music"); };
 
-/*
-document.getElementById('writing-icon').onclick=function() { toggleShown("writing"); };
-document.getElementById('writing-close').onclick=function() { toggleShown("writing"); };
-*/
-
 document.getElementById('wellbeing-icon').onclick=function() { toggleShown("wellbeing") };
 document.getElementById('wellbeing-close').onclick=function() { toggleShown("wellbeing") };
 
@@ -46,3 +70,17 @@ document.getElementById('community-close').onclick=function() { toggleShown("com
 
 document.getElementById('coding-icon').onclick=function() { toggleShown("coding"); };
 document.getElementById('coding-close').onclick=function() { toggleShown("coding"); };
+
+//regardless of which div is open pressing esc will try to close them all
+document.onkeydown=function(evt) {
+    console.log("nappulaa painettu");
+    if("key" in evt) {
+        if(evt.key === "Esc" || evt.key === "Escape") {
+            removeShown();
+        }
+    } else {
+        if(evt.keyCode === 27) {
+            removeShown();
+        }
+    }
+};
